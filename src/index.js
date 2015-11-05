cc9a4bbd66b116b5a784d1b8f8d4e5f27d62bc80
Hiroki Osame <hiroki@weebly.com>
2015-11-05 13:38:48 -0800

    formatting and dependencies

--- src/index.js
+++ src/index.js
@@ -11,96 +11,96 @@ var filter = require('streamfilter');
 var spawn = require('gulp-spawn');
 
 function gulpFontIcon(options) {
-  var inStream = null;
-  var outStream = null;
-  var duplexStream = null;
+	var inStream = null;
+	var outStream = null;
+	var duplexStream = null;
 
-  options = options || {};
-  options.autohint = !!options.autohint;
-  options.formats = options.formats || ['ttf', 'eot', 'woff'];
-  // Generating SVG font and saving her
-  inStream = svgicons2svgfont(options);
-  // Generating TTF font and saving her
-  outStream = inStream
-    .pipe(svg2ttf({
-      clone: -1 !== options.formats.indexOf('svg'),
-      timestamp: options.timestamp,
-    }).on('error', function(err) {
-      outStream.emit('error', err);
-    }))
-  // TTFAutoHint
-    .pipe(cond(options.autohint, function() {
-      var nonTTFfilter = filter(function(file, unused, cb) {
-        cb(file.path.indexOf('.ttf') !== file.path.length - 4);
-      }, {
-        objectMode: true,
-        restore: true,
-        passthrough: true,
-      });
+	options = options || {};
+	options.autohint = !!options.autohint;
+	options.formats = options.formats || ['ttf', 'eot', 'woff'];
+	// Generating SVG font and saving her
+	inStream = svgicons2svgfont(options);
+	// Generating TTF font and saving her
+	outStream = inStream
+		.pipe(svg2ttf({
+			clone: -1 !== options.formats.indexOf('svg'),
+			timestamp: options.timestamp,
+		}).on('error', function(err) {
+			outStream.emit('error', err);
+		}))
+	// TTFAutoHint
+		.pipe(cond(options.autohint, function() {
+			var nonTTFfilter = filter(function(file, unused, cb) {
+				cb(file.path.indexOf('.ttf') !== file.path.length - 4);
+			}, {
+				objectMode: true,
+				restore: true,
+				passthrough: true,
+			});
 
-      return duplexer(
-        { objectMode: true },
-        nonTTFfilter,
-        nonTTFfilter.pipe(spawn({
-          cmd: '/bin/sh',
-          args: [
-            '-c',
-            'cat | ttfautohint --symbol --fallback-script=latn' +
-              ' --windows-compatibility --no-info /dev/stdin /dev/stdout | cat',
-          ],
-        })).pipe(nonTTFfilter.restore)
-      ).on('error', function(err) {
-        outStream.emit('error', err);
-      });
-    }))
-  // Generating EOT font
-    .pipe(cond(
-      -1 !== options.formats.indexOf('eot'),
-      function() {
-        return ttf2eot({ clone: true }).on('error', function(err) {
-          outStream.emit('error', err);
-        });
-      }
-    ))
-  // Generating WOFF font
-    .pipe(cond(
-      -1 !== options.formats.indexOf('woff'),
-      function() {
-        return ttf2woff({ clone: true }).on('error', function(err) {
-          outStream.emit('error', err);
-        });
-      }
-    ))
-  // Generating WOFF2 font
-    .pipe(cond(
-      -1 !== options.formats.indexOf('woff2'),
-      function() {
-        return ttf2woff2({ clone: true }).on('error', function(err) {
-          outStream.emit('error', err);
-        });
-      }
-    ))
-  // Filter TTF font if necessary
-    .pipe(cond(
-      -1 === options.formats.indexOf('ttf'),
-      function() {
-        return filter(function(file, unused, cb) {
-          cb(file.path.indexOf('.ttf') === file.path.length - 4);
-        }, {
-          objectMode: true,
-          passthrough: true,
-        });
-      }
-    ));
+			return duplexer(
+				{ objectMode: true },
+				nonTTFfilter,
+				nonTTFfilter.pipe(spawn({
+					cmd: '/bin/sh',
+					args: [
+						'-c',
+						'cat | ttfautohint --symbol --fallback-script=latn' +
+							' --windows-compatibility --no-info /dev/stdin /dev/stdout | cat',
+					],
+				})).pipe(nonTTFfilter.restore)
+			).on('error', function(err) {
+				outStream.emit('error', err);
+			});
+		}))
+	// Generating EOT font
+		.pipe(cond(
+			-1 !== options.formats.indexOf('eot'),
+			function() {
+				return ttf2eot({ clone: true }).on('error', function(err) {
+					outStream.emit('error', err);
+				});
+			}
+		))
+	// Generating WOFF font
+		.pipe(cond(
+			-1 !== options.formats.indexOf('woff'),
+			function() {
+				return ttf2woff({ clone: true }).on('error', function(err) {
+					outStream.emit('error', err);
+				});
+			}
+		))
+	// Generating WOFF2 font
+		.pipe(cond(
+			-1 !== options.formats.indexOf('woff2'),
+			function() {
+				return ttf2woff2({ clone: true }).on('error', function(err) {
+					outStream.emit('error', err);
+				});
+			}
+		))
+	// Filter TTF font if necessary
+		.pipe(cond(
+			-1 === options.formats.indexOf('ttf'),
+			function() {
+				return filter(function(file, unused, cb) {
+					cb(file.path.indexOf('.ttf') === file.path.length - 4);
+				}, {
+					objectMode: true,
+					passthrough: true,
+				});
+			}
+		));
 
-  duplexStream = duplexer({ objectMode: true }, inStream, outStream);
+	duplexStream = duplexer({ objectMode: true }, inStream, outStream);
 
-  // Re-emit codepoint mapping event
-  inStream.on('glyphs', function(glyphs) {
-    duplexStream.emit('glyphs', glyphs, options);
-  });
+	// Re-emit codepoint mapping event
+	inStream.on('glyphs', function(glyphs) {
+		duplexStream.emit('glyphs', glyphs, options);
+	});
 
-  return duplexStream;
+	return duplexStream;
 }
 
 module.exports = gulpFontIcon;
